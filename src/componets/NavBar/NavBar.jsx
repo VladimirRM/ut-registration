9e22a131b93744ff27b844fcd5bc792080129185
Vladimir RM <shvdrummer@gmail.com>
2023-05-11 15:11:52 +0300

    Add many style

--- src/componets/NavBar/NavBar.jsx
+++ src/componets/NavBar/NavBar.jsx
@@ -3,15 +3,17 @@ import './NavBar.less'
 import Logo from '../../assets/navbar-logo.svg'
 
 const NavBar = () => {
-  return (
-    <div className='navbar'>
-        <img src={Logo} alt="" />
-        <div className="navbar__header">MERN Cloud</div>
-        <div className="navbar__login">Войти </div>
-        <div className="navbar__registration">Регистрация</div>
-      
-    </div>
-  )
+    return (
+        <div className='navbar'>
+            <div className="container">
+                <img src={Logo} alt="" />
+                <div className="navbar__header">MERN Cloud</div>
+                <div className="navbar__login">Войти </div>
+                <div className="navbar__registration">Регистрация</div>
+            </div>
+        </div>
+
+    )
 }
 
 export default NavBar
