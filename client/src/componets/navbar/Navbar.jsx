9d1762c865f7206daa93dcc54ca1ad58fbf2f918
Vladimir RM <shvdrummer@gmail.com>
2023-05-12 12:45:09 +0300

    Correct css position

--- client/src/componets/navbar/Navbar.jsx
+++ client/src/componets/navbar/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import './Navbar.less'
-import Logo  from'../../assets/img/lofo.svg'
+import './Navbar.scss'
+import Logo from '../../assets/img/lofo.svg'
 
 const Navbar = () => {
     return (
         <div className='navbar'>
-        <img src={Logo} alt="" />
-        <div className="navbar__header">MERN CLOUD</div>
-        <div className="navbar__login">Войти</div>
-        <div className="navbar__registration">Регистрация</div>
+            <div className="container">
+                <img src={Logo} alt="" />
+                <div className="navbar__header">MERN CLOUD</div>
+                <div className="navbar__login">Войти</div>
+                <div className="navbar__registration">Регистрация</div>
+            </div>
+
         </div>
     );
 };
