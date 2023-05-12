7009d0dc16ede56772b532778ee3a7da274854bc
Vladimir RM <shvdrummer@gmail.com>
2023-05-12 14:45:05 +0300

    import BrowserRouter

--- client/src/componets/App.jsx
+++ client/src/componets/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {BrowserRouter} from "react-router-dom"
 
 import Navbar from './navbar/Navbar';
 
 function App() {
   return (
     <div className="app">
+      <BrowserRouter></BrowserRouter>
    <Navbar/>
     </div>
   );
