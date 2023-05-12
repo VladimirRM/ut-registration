685ad63f32c3fb80a9b7991bf85348b03d9d5596
Vladimir RM <shvdrummer@gmail.com>
2023-05-12 14:54:22 +0300

    Create dir input

--- client/src/componets/App.jsx
+++ client/src/componets/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter, Route} from "react-router-dom"
+import Switch from 'react-dom'
+import Registration from './registration/Registration';
 
 import Navbar from './navbar/Navbar';
 
 function App() {
   return (
+    <BrowserRouter>
+
     <div className="app">
-      <BrowserRouter></BrowserRouter>
-   <Navbar/>
+           <Navbar/>
+      <Switch>
+        <Route path='/registration' component={Registration}/>
+      </Switch>
     </div>
+    </BrowserRouter>
+
   );
 }
 
---
+++ client/src/componets/registration/Registration.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Registration.scss'
+
+const registaration = () => {
+    return (
+        <div className='registration'>
+            <div className="registration__header"></div>
+            
+        </div>
+    );
+};
+
+export default registaration;
