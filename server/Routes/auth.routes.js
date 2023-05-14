fb03dac71fde0fe2adb1131d3e77677d72b55dcc
Vladimir RM <shvdrummer@gmail.com>
2023-05-14 11:15:07 +0300

    start created login

--- server/Routes/auth.routes.js
+++ server/Routes/auth.routes.js
@@ -5,19 +5,48 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const {check,validationResult} =require("express-validator")
 
-router.post("/registration",(req,res)=>{
+router.post("/registration", 
+[
+   check("email", "uncorrect email").isEmail(),
+   check("password", "Password must be longer than 3 and and shorter than 12").isLength({min:3,max:12}),
+],
+async (req,res)=>{
     try{
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({message:"Uncorrect request",errors})
+        }
+
      const {email,password} = req.body
-      const candidate =  User.findOne({email})
+
+      const candidate = await User.findOne({email})
       if(candidate){
         return res.status(400).json({message:" User with email ${email} already exist "})
 
       }
 
-      
-      const user = new User({email})
+      const hashPassword =  await bcrypt.hash(password, 8)
+      const user = new User({email, password:hashPassword})
+       await user.save()
+       return res.json({message:"User was created"})
 
     }catch(e){
+        console.log(e)
+        res.send({message:"Server error"})
+    }
+})
+
+
+router.post("/login" ,
+async (req,res)=>{
+    try{
+     const {email,password} =req.body
+     const user = new User.findOne({email})
+     if(!user){
+        return res.status(400).json({message:"User not found"})
+     } 
+  
+      }catch(e){
         console.log(e)
         res.send({message:"Server error"})
     }
