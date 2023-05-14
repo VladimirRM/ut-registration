46dc7648da8f1207511510da11a98cb8254e2d5b
Vladimir RM <shvdrummer@gmail.com>
2023-05-14 10:53:04 +0300

    Add hash for password

--- server/Routes/auth.routes.js
+++ server/Routes/auth.routes.js
@@ -5,24 +5,25 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const {check,validationResult} =require("express-validator")
 
+router.post("/registration",(req,res)=>{
+    try{
+     const {email,password} = req.body
+      const candidate =  User.findOne({email})
+      if(candidate){
+        return res.status(400).json({message:" User with email ${email} already exist "})
 
-router.post('/login', async (req,res) => {
-       try{
-       const {email,password} =req.body
-       const user  = await User.findOne({email})
-       if(!user){
-       return res.status(400).json({message: "User not found"})
-}
-    const isPassValid = bcrypt.compareSync(password,user.password)
-    if(!isPassValid){
-        return res.status(400).json({message: "Invalid password"})
+      }
 
-    }
-   
+      
+      const user = new User({email})
+
     }catch(e){
         console.log(e)
-        res.send({message: "Sever error"})
+        res.send({message:"Server error"})
     }
 })
+
+
+
 
 module.exports = router
