fecec785ffcd64a0d8ba27ddbc5c9deb7b841551
Vladimir RM <shvdrummer@gmail.com>
2023-05-14 18:00:10 +0300

    add file reducers

---
+++ client/src/reducers/index.js
@@ -0,0 +1,9 @@
+import {applyMiddleware, combineReducers,createStore} from 'redux'
+import {composeWithDevTools } from 'redux-devtools-extension'
+
+const rootReducer = combineReducers({
+
+})
+
+
+export const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)))
