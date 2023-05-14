82cc58e49d291979ffd22dabcdd2e57ec0a2c3f4
Vladimir RM <shvdrummer@gmail.com>
2023-05-14 10:00:15 +0300

    Import jwt token

--- server/Routes/auth.routes.js
+++ server/Routes/auth.routes.js
@@ -2,14 +2,22 @@ const Router = require('express')
 const User = require('../models/User')
 const router =  new Router()
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const {check,validationResult} =require("express-validator")
 
 
 router.post('/login', async (req,res) => {
+       try{
+       const {email,password} =req.body
+       const user  = await User.findOne({email})
+       if(!user){
+       return res.status(400).json({message: "User not found"})
+}
+    const isPassValid = bcrypt.compareSync(password,user.password)
+    if(!isPassValid){
+        return res.status(400).json({message: "Invalid password"})
 
-  
-   try{
-
+    }
    
     }catch(e){
         console.log(e)
